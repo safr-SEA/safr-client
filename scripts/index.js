f0b832ad4a0a9283f78ce38d60bd84fb4a9ddf9c
Jermaine Walker <jermainemwalker@gmail.com>
2018-05-25 16:55:26 -0700

    Initial setup.

--- scripts/index.js
+++ scripts/index.js
@@ -3,5 +3,12 @@
 app = app || {};
 
 ( function( module ) {
+  let productionApiUrl = 'https://safr-sea.herokuapp.com/';
+  let developmentApiUrl = 'http://localhost:3000';
 
+  module.isProductioon = /^(?!localhost|127)/.test( window.location.hostname );
+  module.ENVIRONMENT = {
+    apiURl: module.isProduction ? productionApiUrl : developmentApiUrl
+  };
+  
 } )( app );
