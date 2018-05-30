ef2422f4867b328c9feeafc091c3b2321803bab8
Jermaine Walker <jermainemwalker@gmail.com>
2018-05-30 16:56:15 -0700

    Updated index.js with double slash.

--- scripts/index.js
+++ scripts/index.js
@@ -3,7 +3,7 @@
 var app = app || {};
 
 ( function( module ) {
-  let productionApiUrl = 'https://safr-sea.herokuapp.com/';
+  let productionApiUrl = 'https://safr-sea.herokuapp.com';
   let developmentApiUrl = 'http://localhost:3000';
 
   module.isProduction = /^(?!localhost|127)/.test( window.location.hostname );
