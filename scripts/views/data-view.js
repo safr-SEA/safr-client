2163a5a6d140172be687a25e43a4a8b38132abf2
Karen Mac <karenbtlai@gmail.com>
2018-05-31 01:25:01 -0700

    changed map styling and animate data vis

--- scripts/views/data-view.js
+++ scripts/views/data-view.js
@@ -4,11 +4,17 @@ var app = app || {};
 
 (function(module) {
 
+  $('#close').on('click', () => $('#data-vis').animate({top:'100vh'}, 'ease'));
   let initPlotly = (result) => {
+
+    $('#data-vis').animate({
+      top: '0'
+    }, 'ease');
+
     let crimes = result.map((obj) => obj.summarized_offense_description);
     let crimeUnique = crimes.filter((v, i, a) => a.indexOf(v) === i);
     let crimeCount = crimeUnique.map((ele) => crimes.reduce((v, i) => (v + (i == ele)), 0));
-
+    
     var data = [{
       values: crimeCount,
       labels: crimeUnique,
@@ -16,11 +22,13 @@ var app = app || {};
     }];
 
     var layout = {
-      height: 400,
-      width: 700
+      height:480,
+      width:480,
+      showlegend: true,
+      legend: {"orientation": "h"}
     };
 
-    Plotly.newPlot('data-vis', data, layout);
+    Plotly.newPlot('data-vis', data, layout, {displayModeBar:false});
   };
 
   module.initPlotly = initPlotly;
