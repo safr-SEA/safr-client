99e325b8d7df6238bc62b5ab96f084baf7a947c2
Jermaine Walker <jermainemwalker@gmail.com>
2018-05-31 15:56:30 -0700

    Landing page WIP.

--- scripts/views/view.js
+++ scripts/views/view.js
@@ -2,20 +2,29 @@
 
 var app = app || {};
 
-(function(module) {
+( function ( module ) {
 
   const view = {};
 
-  view.initReport = function() {
-    $.get(`${app.ENVIRONMENT.apiUrl}/data/sea-gov`)
-      .then(console.log)
-      .catch(console.error)
+  view.initReport = function () {
+    $.get( `${ app.ENVIRONMENT.apiUrl }/data/sea-gov` )
+      .then( console.log )
+      .catch( console.error )
   }
 
   view.initLoginPage = function () {
-    
+    $( '.container' ).hide();
+    $( '#nav-menu' ).hide();
+    $( '#login-page' ).fadeIn( 'slow' );
   }
+
+  view.initAdminPage = function () { 
+    $( '.container' ).hide();
+    $( '#admin-page' ).fadeIn( 'slow' );
+   }
 
   module.view = view;
 
-})(app);
+} )( app );
+
+app.view.initLoginPage();
